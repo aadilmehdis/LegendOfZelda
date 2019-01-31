5fa8f63d567b83352d7354994f24d2212da0dd48
AadilMehdi Sanchawala <aadilmehdi.s@students.iiit.ac.in>
2019-01-31 13:45:36 +0530

    Building the Game & Updating Controls and Istructions in README.md

--- README.md
+++ README.md
@@ -5,5 +5,29 @@ The game is an OpenGL C++ implementation of a fighter plane game in a #D World.
 
 ## Instructions
 
+### Building the Game
+
+- Run the following commands in the main directory
+  - `mkdir build && cd build`
+  - `cmake ..`
+  - `make all`
+  - `./graphics_asgn1`
+
 ## Controls
 
+- **SPACEBAR**
+  - Control the altitude of the Plane. Press the SPACEBAR to increase the altitude of the plane. On releasing the key the plane loses altitude.
+- **W**
+  - Increase the forward velocity of the plane
+- **A**
+  - Tilt the plane to the left
+- **D** 
+  - Tilt the plane to the right
+- **Q** 
+  - Rotate the plane to the counter clockwise
+- **E** 
+  - Rotate the plane to the clockwise
+- **Mouse Left Click**
+  - Release Missiles
+- **Mouse Right Click**
+  - Drop Bombs
---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/ball.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/color.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/input.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/main.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/nonedit.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/other_handlers.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/src/timer.cpp" "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/AntTweakBar-1.16/include"
+  "../external/glfw-3.1.2/include"
+  "../external/glm-0.9.7.1"
+  "../external/glew-1.13.0/include"
+  "../external/assimp-3.0.1270/include"
+  "../external/bullet-2.81-rev2613/src"
+  "../."
+  "/usr/local/Cellar/glfw/3.2.1/include"
+  "/usr/local/Cellar/glew/2.1.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/external/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/aadilmehdi/Semester4/Graphics/Assignments/Assignment2/LegendOfZelda/build/external/CMakeFiles/GLEW_1130.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
